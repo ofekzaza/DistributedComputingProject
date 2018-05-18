f0e44e2574d696b79bc4b0797cff46a2f974db16
ofekzaza <ofekeshet@gmail.com>
2018-05-18 18:30:54 +0300

    BUGS

--- communicator.py
+++ communicator.py
@@ -1,15 +1,15 @@
-from abc import ABCMeta
 import socket
-import time
 
-class Comunicatora(metaclass=ABCMeta):
+
+class Communicator():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # creating the socket
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # added in order to fix the bug the may accure
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
     def __init__(self):
         self.s.bind(('', 9999))
 
-    def send(self, origin:str, target:str, returnTarget:str, msg:str):
+    def send(self, origin:str, target:str, returnTarget:str, msg:str) -> bool:
         "the function is sending data "
         message: str = "Origin:"+origin+",Target:"+target+",ReturnTo:"+returnTarget+",Mission:"+msg #set the message in a string format
         self.s.sendto(message.encode('ascii'), ('<broadcast>', 9999))
@@ -26,7 +26,7 @@ class Comunicatora(metaclass=ABCMeta):
 
     def __del__(self):
         self.s.close()
-        print(self.id, 'died')
+        print('Communicator died, RIP')
 
 
 
